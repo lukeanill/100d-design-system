9e74d576a782fa480fcc35ce1d2516c418538df7
lukeatbreezy <luke@breezy.com>
2026-08-24 22:45:59 +0100

    Fix Bubble Background hoisting (export name didn't match title's last segment)

--- packages/ui/src/components/animate-ui/components/backgrounds/bubble.stories.tsx
+++ packages/ui/src/components/animate-ui/components/backgrounds/bubble.stories.tsx
@@ -3,4 +3,4 @@ import { BubbleBackground as BubbleBackgroundImpl } from "./bubble"
 
 export default { title: "Components/Bubble Background", component: BubbleBackgroundImpl }
 
-export const Bubble = (args: ComponentProps<typeof BubbleBackgroundImpl>) => <BubbleBackgroundImpl {...args} />
+export const BubbleBackground = (args: ComponentProps<typeof BubbleBackgroundImpl>) => <BubbleBackgroundImpl {...args} />
